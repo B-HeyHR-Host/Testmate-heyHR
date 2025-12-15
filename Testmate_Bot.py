7b1c828d1214c4eea3cd8280f8250dae7735924b
Billy Horgan <Billy@hrlogic-services.com>
2025-12-15 20:08:15 +0000

    FIX

--- Testmate_Bot.py
+++ Testmate_Bot.py
@@ -10,7 +10,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import TextLoader, PyPDFLoader
 from langchain_core.documents import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -68,7 +68,7 @@ logo_path = Path("logo.png.png")
 col1, col2, col3 = st.columns([1, 2, 1])
 
 with col2:
-    st.image(logo, width=500)
+st.image(logo,width=500)
 st.markdown("<h2 style='text-align: center;'></h3>", unsafe_allow_html=True)
 st.markdown("I am here to help with your HR queries")
 
