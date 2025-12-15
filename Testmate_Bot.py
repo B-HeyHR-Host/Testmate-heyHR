89d7e4399648fe4a0ef35840e76614336c0c4b3d
Billy Horgan <Billy@hrlogic-services.com>
2025-12-15 19:46:59 +0000

    FIX

--- Testmate_Bot.py
+++ Testmate_Bot.py
@@ -4,8 +4,8 @@ import pandas as pd
 from PIL import Image
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.chains.retrieval_qa.base import RetrievalQA
-from langchain.vectorstores.base import VectorStoreRetriever
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import TextLoader, PyPDFLoader
 from langchain_core.documents import Document
 from langchain.text_splitter import CharacterTextSplitter
